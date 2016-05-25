f8d2c99b635056daaeeab6a7352222bc28056f72
Sophie Tan <tansipei@gmail.com>
2016-05-25 11:13:54 -0400

    twitter completed

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,20 +1,14 @@
 class ProfilesController < ApplicationController
 
-<<<<<<< Updated upstream
 def show
   @user = User.find(params[:id])
 end
 
-=======
->>>>>>> Stashed changes
 def index
   @users = User.all
 end
 
-<<<<<<< Updated upstream
-=======
 def show
   @user = User.find(params[:id])
   end
->>>>>>> Stashed changes
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+  has_many :likes
 
   accepts_nested_attributes_for :user
 
